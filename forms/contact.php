493af248f97bd60de41b6cb3c5da063dad942df0
Nuran Chandrasekara <nuranhesara321@gmail.com>
2025-03-29 20:57:53 +0530

    update php

--- forms/contact.php
+++ forms/contact.php
@@ -17,12 +17,7 @@
   $contact->from_email = $_POST['email'];
   $contact->subject = $_POST['subject'];
 
-  $contact->smtp = array(
-    'host' => 'smtp.gmail.com',  // Change this to your SMTP provider
-    'username' => 'nuranhesara321@gmail.com',  // Your email address
-    'password' => 'ishani2022',  // Your email password or App Password
-    'port' => '587'  // Usually 465 for SSL, 587 for TLS
-);
+
 
 
   $contact->add_message( $_POST['name'], 'From');
